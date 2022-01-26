df4cba4f54d3db0bf96fc1664e827a33340a4abe
sushil-softserv <sushil@softserv.in>
2022-01-26 21:51:37 +0530

    Step-6

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
+import 'package:mynotes/utilities/show_error_dialog.dart';
 
 import 'package:mynotes/constants/route.dart';
 
@@ -69,11 +69,20 @@ class _LoginViewState extends State<LoginView> {
                 );
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
-                  devtools.log('User not found.');
+                  //devtools.log('User not found.');
+                  await showErrorDialog(context, 'User not found.');
                 } else if (e.code == 'wrong-password') {
-                  devtools.log('Invalid password!');
-                  devtools.log(e.toString());
+                  await showErrorDialog(context, 'Invalid password!');
+                  // devtools.log('Invalid password!');
+                  // devtools.log(e.toString());
+                } else {
+                  await showErrorDialog(context, 'Error: ${e.code}');
                 }
+              } catch (e) {
+                await showErrorDialog(
+                  context,
+                  e.toString(),
+                );
               }
             },
             child: const Text('Login'),
